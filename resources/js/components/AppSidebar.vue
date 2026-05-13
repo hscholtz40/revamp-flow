c17c0b27d04bdfd446255d3b2367b354b3790cf7
Karl <kardoskarlheinz11@gmail.com>
2026-05-13 16:49:06 +0200

    added theme settings in profile

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -377,13 +377,18 @@ const filteredNavItems = computed(() => {
         <SidebarFooter class="border-t border-sidebar-border/60 pt-3">
             <NavFooter v-if="userType !== 'limited' && filteredFooterNavItems.length > 0" :items="filteredFooterNavItems" />
             <!-- Default Logo above user menu when company logo is uploaded -->
-            <div v-if="currentCompany?.logo_path" class="mb-3 px-2">
                 <img
+                    v-if="currentCompany?.logo_path"
+                    :src="`/storage/${currentCompany.logo_path}`"
+                    :alt="currentCompany.name"
+                    class="w-full h-auto max-h-16 object-contain rounded"
+                />
+                <img
+                    v-else
                     src="/logo-big.webp"
                     alt="JobCardOnline"
                     class="w-full h-auto max-h-12 object-contain opacity-80 hover:opacity-100 transition-opacity"
                 />
-            </div>
             <NavUser />
         </SidebarFooter>
     </Sidebar>
